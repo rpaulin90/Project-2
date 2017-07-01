fa40b12c4e340f7dba54c8c6862425c115b8a83f
Steven Quast <smquast@gmail.com>
2017-07-01 07:46:11 -0500

    Added items to the profile

--- routes/profile_routes.js
+++ routes/profile_routes.js
@@ -12,23 +12,48 @@ module.exports = function(app)
 	{
 		console.log("Line 13");
 		console.log(req.session.user_id);
+
+		if(req.session.user_id == undefined)
+		{
+			res.render("profile", {user:"",base:"",items:""});
+		}
+
 		db.User.findOne(
 		{
-			where: { firebase_id: req.body.UserId }
-		}).then(function(results)
+			where: { firebase_id : req.session.user_id }
+		}).then(function(userResults)
 		{
-			res.render("profile");
-			// console.log(results);
-			// console.log("req.body.UserId: " + req.body.UserId);
-			// console.log("results: " + results.dataValues.id);
+			var user = userResults.dataValues;
 
-			// db.Item.findAll(
-			// {
-			// 	where: { UserId: results.dataValues.userId }
-			// }).then(function(listResults)
-			// {
-			// 	console.log("results: " + listResults);
-			// });
+			db.Base.findOne(
+			{
+				where: { id : user.BaseId }
+			}).then(function(baseResults)
+			{
+				var base = baseResults.dataValues;
+
+				console.log("userid: " + user.id);
+				db.Item.findAll().then(function(itemResults)
+				{
+					var items = [];
+					for(var i = 0; i < itemResults.length; i++)
+					{
+						items.push(itemResults[i].dataValues);
+						items[i].BaseName = base.base_name;
+						items[i].UserName = user.name;
+						items[i].UserRank = user.rank;
+					}
+
+					console.log(items);
+
+					res.render("profile",
+					{
+						user : user,
+						base : base,
+						items : items
+					});
+				});	
+			});
 		});
 	});
 };
