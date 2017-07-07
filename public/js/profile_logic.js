4f78499ca290145cc695e1e87bb8f2096950069a
Steven Quast <smquast@gmail.com>
2017-07-06 20:34:23 -0500

    added code to delete posts and display the correct image When you delete a post, it will inform you of the deletion and then refresh the page

--- public/js/profile_logic.js
+++ public/js/profile_logic.js
@@ -1,6 +1,12 @@
-// $("#my_posts_btn").on("click",function(event){
-
-// 	event.preventDefault();
-//     console.log(current_user_id);
-
-// });
+$(".delete-post-btn").on("click",function(event)
+{
+	var itemId = event.target.getAttribute("data-id");
+	event.target.disabled = true;
+	
+	$.post("/api/delete_post", { where : { id : itemId } }, function(data)
+	{
+		event.target.setAttribute("class", "btn btn-success text-center delete-post-btn");
+		event.target.innerHTML = "Post Deleted";
+		location.reload();
+	});
+});
